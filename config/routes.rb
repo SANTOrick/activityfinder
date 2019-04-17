39318620536d4b472e48b88045101b709e9d4329
myrtokonst <myrtokonst@gmail.com>
2019-04-17 10:35:47 +0100

    uekfjbsd

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :users
   resources :questions, except: [:create]
   post '/questions/:id/answer', to: 'questions#answer', as: :answer_question
-  root to: 'users#new'
+  root to: 'users#main_page'
   get "/profile", to: "users#profile"
   get "/login_form", to: "sessions#login_form"
   post "/login", to: "sessions#login", as: "login"
