07f26c4e39f9958f75bfafa64747eaadbe779e6b
myrtokonst <myrtokonst@gmail.com>
2019-04-17 10:42:55 +0100

    working routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'users#main_page'
+  get "/profile", to: "users#profile"
+  get "/login_form", to: "sessions#login_form"
+  post "/login", to: "sessions#login", as: "login"
   get '/users/main_page', to: 'users#main_page', as: :main_page
   get '/activities/index_museum', to: 'activities#index_museum', as: :index_museum
   post '/questions/:id/answer', to: 'questions#answer', as: :answer
@@ -9,8 +12,6 @@ Rails.application.routes.draw do
   resources :users
   resources :questions, except: [:create]
   post '/questions/:id/answer', to: 'questions#answer', as: :answer_question
-  get "/profile", to: "users#profile"
-  get "/login_form", to: "sessions#login_form"
-  post "/login", to: "sessions#login", as: "login"
+
 
 end
